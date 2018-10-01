ce2ed9f9c877109c0acff6ee915798cf8ed16080
LiamCosgrove <cosi272@gmail.com>
2018-10-01 20:37:28 +1000

    refactored to use requests

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # COMS4200
+
+Please note: It is expected that you will manually import the Kibana Dashboard into Kibana
+
+The file to import is called "kibana_objects.json"
--- bridge.py
+++ bridge.py
@@ -10,6 +10,7 @@ import math
 import requests 
 import pygeohash as pgh
 
+from requests.auth import HTTPBasicAuth
 from datetime import datetime as dt
 from subprocess import PIPE, Popen
 
@@ -21,8 +22,6 @@ path_to_kibana_ddl = "./kibana_objects.json"
 path_to_elastic_index_schemas = "./elastic_index_schemas.json"
 path_to_elastic_create_index = "./elastic_create_index.json"
 json_header = {"Content-Type" : "application/json"}
-kibana_header = {"kbn-xsrf" : "true"}
-kibana_header.update(json_header)
 
 # lat and longitude ranges for st lucia
 long_start = 153.006618
@@ -50,14 +49,6 @@ elastic_indexes = [elastic_index for onos_resource, elastic_index in onos_to_ela
 parser = argparse.ArgumentParser()
 parser.add_argument('--drop_all', help='Drops all existing elasticsearch indexes.')
 args = vars(parser.parse_args())
-
-with open(path_to_kibana_ddl, "r") as f:
-	kibana_ddl = json.loads(f.read())
-kibana_ddl_dict = {}
-kibana_ddl_dict['objects'] = json.loads(kibana_ddl)
-kibana_ddl = json.dumps(kibana_ddl_dict)
-
-sys.exit(1)
 
 with open(path_to_elastic_create_index, "r") as f:
 	create_index = f.read()
@@ -145,24 +136,6 @@ for elastic_index, schema in elastic_index_to_schema.iteritems():
 	req = requests.put(url, data = schema, headers = json_header)
 	logger.info(pdumps({"content":req.content, "headers" : str(req.headers), "data" : schema}))
 
-logger.info("About to deploy Kibana dashboard.")
-time.sleep(2)
-
-# Import dashboard into Kibana, overwrite any existing dashboard with same ID
-proc = Popen('curl -X POST "http://{0:s}:{1:s}/api/kibana/dashboards/import?force=true"'.format(kibana['host'],kibana['port']) + \
-" -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d'\n" + kibana_ddl + "\n'", stdout=PIPE, stderr=PIPE, shell=True)
-(out, err) = proc.communicate()
-if proc.returncode <> 0:
-	logger.error(err)
-	sys.exit(1)
-print out
-logger.info("Succesfully deployed Kibana Dashboard.")
-time.sleep(2)
-
-# now that elasticsearch and kibana services are running, and required indexes exist, scrape ONOS rest api every 30 seconds,
-# enrich data with extract timestamp, and post document to relevant index
-# The script may fail if elasticsearch or kibana services become unavailable.
-
 logger.info("About to begin polling ONOS REST API")
 ingest_start_time = int(time.time())
 # Store last snapshot of flow in dictionary.
@@ -173,27 +146,23 @@ while True:
     # request each resource in the onos_to_elastic mapping, and post to elastic index
     # Set the extract timestamp
     extract_ts = int(time.time())
-    for resource, index in onos_to_elastic.iteritems():
-	if index != "flows":
+    for resource, elastic_index in onos_to_elastic.iteritems():
+	if elastic_index != "flows":
 		continue
         print '=' * 40 + ">"
-        logger.info("ONOS API Resource request:\n" + \
-        "curl -H 'Accept: application/json' -u onos:rocks -X GET 'http://{0:s}:{1:s}/onos/v1{2:s}'".format(onos['host'], onos['port'], resource))
-        proc = Popen("curl -H 'Accept: application/json' -u onos:rocks -X GET 'http://{0:s}:{1:s}/onos/v1{2:s}'".format(onos['host'], onos['port'], resource), stdout=PIPE, stderr=PIPE, shell=True)
-        (out, err) = proc.communicate()
-        if proc.returncode <> 0:
-            print err
-        results_dict = json.loads(out)
+	url = "{0:s}/onos/v1{1:s}".format(onos_url, resource)
+	req = requests.get(url,  auth=HTTPBasicAuth('onos', 'rocks'))
+        results_dict = json.loads(req.content)
 	
 	# Create an individual document for each child object returned in the response object.
-	for child_object in results_dict[index]:
+	for child_object in results_dict[elastic_index]:
 		# Enrich the data with a timestamp
 		child_object['extract_timestamp'] = extract_ts
 		# Enrich the data with a count since the start of the ingest process, used because Kibana makes it easier to filter by long range rather than date range??
 		child_object['time_passed_seconds'] = extract_ts - ingest_start_time
 
 		# Enrich data with location
-		if index == "flows":
+		if elastic_index == "flows":
 			#If device not already assigned a location, generate one
 			if child_object['deviceId'] not in device_locations:
 				
@@ -205,13 +174,12 @@ while True:
 			else:
 				#get the device location
 				child_object['location'] = device_locations[child_object['deviceId']]
-		print str(current_locations)
 
 		# Even though a flow may not be permanent and may only have a time to live of 10 seconds before it is removed from the flow tables, the flow if it is reinstalled
 		# will have the same flow ID. The heatmap view of aggregate flow rule bytes per device requires a field which gives bytes per poll increment rather than a total.
 		# This mean we be visualising the deltas, rather than the total, so we should enrich the data with a new field called bytes_delta for each flow.
 		# This field therefore will only be populated after we have recieved at least two snapshots of a flow.		
-		if index == "flows":
+		if elastic_index == "flows":
 			# If first time we have seen the flow rule
 			if child_object['id'] not in flows:
 				flows[child_object['id']] = child_object
@@ -228,14 +196,10 @@ while True:
 		logger.info("Succesfully retrived object from ONOS resource " + resource +":\n" + json.dumps(child_object, sort_keys=True, indent=4, separators=(',', ': ')))
 
 		# now post the data to elasticsearch
-		proc = Popen("curl -X POST \"{0:s}:{1:s}/{2:s}/_doc/\" -H 'Content-Type: application/json' -d'".format(elastic["host"],elastic["port"],index) + json.dumps(child_object) + "'", stdout=PIPE, stderr=PIPE, shell=True)
-		(out, err) = proc.communicate()
-		if proc.returncode <> 0:
-		    print err
-		logger.info(out)
-		logger.info(err)
+		url = "{0:s}/{1:s}/_doc/".format(elastic_url, elastic_index)
+		req = requests.post(url, headers = json_header, data = json.dumps(child_object))
 		# Pretty print the results of the insertion into Elasticsearch
-		logger.info("Succesfully inserted document from ONOS resource " + resource + " into Elasticsearch:\n" + json.dumps(json.loads(out), sort_keys=True, indent=4, separators=(',', ': ')))
+		logger.info("Succesfully inserted document from ONOS resource " + resource + " into Elasticsearch:\n" + pdumps(child_object))
 		print "<" + '=' * 40
 
 	# Although flow objects will retain the same ID when readded, the bytes counter is reset to zero, therefore we must purge any old flow objects,
